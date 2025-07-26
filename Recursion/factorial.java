a80f2a7e0a02732247d57205aa554de1f39fd3cd
Shubhika Singh <singh0803.sss@gmail.com>
2025-07-27 02:02:45 +0530

    modified factorial program

--- Recursion/factorial.java
+++ Recursion/factorial.java
@@ -5,25 +5,16 @@ import java.util.Scanner;
 public class factorial {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n =  sc.nextInt();
-        int result = fact(n);
-        System.out.println(result);
-//        int fact=n;
-//        for(int i=0; i<n;i++){
-//            fact=fact*(n-1);
-//            n=n-1;
-//        }
-//        System.out.println(fact);
+        int N = sc.nextInt();
+        int fact = fact(N);
+        System.out.println(fact);
     }
 
     public static int fact(int N) {
-        if(N<1){
+        if (N<1){
             return 1;
         }
-        int result= N;
-        result = result*(fact(N-1));
-        return(result);
-
-
+        int res=(N*fact(N-1));
+        return res;
     }
 }
