06868f6b91a1323e1c172df623ea8e28b3b29933
Shubhika Singh <singh0803.sss@gmail.com>
2025-07-27 20:48:59 +0530

    Added new programs for factorial and fibonacci

--- Recursion/NthFibonnaci.java
+++ Recursion/NthFibonnaci.java
@@ -4,16 +4,18 @@ import java.util.Scanner;
 
 public class NthFibonnaci {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in) ;
-        int N = sc.nextInt();
-        int Ans= fibo(N);
-        System.out.println(Ans);
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    int val=fibo(N);
+    System.out.println(val);
+
     }
 
-    public static int fibo(int N) {
-            if(N==0 || N==1){
-                return  N;
-            }
-            return fibo(N-1)+fibo(N-2);
+    public static int fibo(int n) {
+        if(n<=1){
+            return n;
+        }
+        int fibs = fibo(n-1)+fibo(n-2);
+        return fibs;
     }
 }
