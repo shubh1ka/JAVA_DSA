944beddd6c5086d0bd1b079582c3fdfcb557f6c1
Shubhika Singh <singh0803.sss@gmail.com>
2025-07-28 08:17:42 +0530

    Wrote printing i 10 n with recursion programme

--- Recursion/OnetoN.java
+++ Recursion/OnetoN.java
@@ -6,13 +6,15 @@ public class OnetoN {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        print(1,n);
+        int i=0;
+        printN(n);
     }
 
-    public static void print(int x, int n) {
-        if(x>n) return;
-        System.out.println(x);
-        print(x+1,n);
-
+    public static void  printN(int n) {
+        if(n==0){
+            return ;
+        }
+        printN(n-1);
+        System.out.println(n);
     }
 }
